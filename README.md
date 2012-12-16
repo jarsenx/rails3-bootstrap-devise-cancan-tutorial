627adfd4a2a379a301c547eace06e91710c8ee17
Joe Arsenault <jarsen@earthlink.net>
2012-12-16 14:33:50 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Rails Tutorial for Devise with CanCan and Twitter Bootstrap
+
+This is a tutorial for learning Rails and
+other technologies. You can find the tutorial at:
+[*RailsApps Project*] 
+(http://railsapps.github.com/tutorial-rails-bootstrap-devise-cancan.html) 
+by [Daniel Kehoe].
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3BootstrapDeviseCancan::Application.config.secret_token = '6ae9b8ee705c1a54dca9d157a0e612d78162d9c6de728b885b5925637c47dce6a837e51c5f9e72b75cca3a8d05df44e3828dd6a9a2aecbfd076c22165c03fd73'
